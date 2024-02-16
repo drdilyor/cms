785dedf68ea346eded50df8ea3481bae583fb4d1
drdilyor <drdilyor@outlook.com>
2024-02-16 15:05:51 +0500

    change update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 git pull
 killall -r '^cms[A-Z]'
-echo cms | sudo -S python3 prerequisites.py install
+sudo python3 prerequisites.py --yes --cmsuser=cmsuser install
 nohup cmsResourceService -a 3 &
