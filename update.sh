1b94acbabf51f45309f6c2132d596d5be30af8c5
drdilyor <drdilyor@outlook.com>
2024-02-16 15:28:46 +0500

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-git pull
-killall -r '^cms[A-Z]'
-sudo python3 setup.py install || exit 1
-sudo python3 prerequisites.py --yes --cmsuser=cmsuser install || exit 1
-nohup cmsResourceService -a 3 &
+if [ -z $SECOND_RUN ]; then
+    git pull
+    export SECOND_RUN=1
+    exec ./update.sh "$@"
+else
+    killall -r '^cms[A-Z]'
+    sudo python3 setup.py install || exit 1
+    sudo python3 prerequisites.py --yes --cmsuser=cmsuser install || exit 1
+    nohup cmsResourceService -a 3 &
+    echo "after nohup"
+fi
