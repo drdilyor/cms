5f953107984b2614086e7ca96e59b4dd690e40b1
drdilyor <drdilyor@outlook.com>
2024-02-16 15:22:20 +0500

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 git pull
 killall -r '^cms[A-Z]'
+echo "setup..."
 sudo python3 setup.py install || exit 1
+echo "prereq..."
 sudo python3 prerequisites.py --yes --cmsuser=cmsuser install || exit 1
+echo "nohup..."
 nohup cmsResourceService -a 3 &
