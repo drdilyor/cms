d5ec083c33b20c81e542a4ec321e730c30a45927
drdilyor <drdilyor@outlook.com>
2024-02-18 15:31:13 +0500

    update

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 sudo python3 setup.py install || exit 1
 sudo python3 prerequisites.py --yes --cmsuser=cmsuser install || exit 1
 killall -r '^cms[A-Z]'
-nohup cmsResourceService -a 40 & disown
+nohup cmsResourceService -a 7 & disown
 sleep 0.5s
