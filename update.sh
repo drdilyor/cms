557ed320e5e26a7fd3d7b352c204d282cfc5395b
drdilyor <drdilyor@outlook.com>
2024-04-21 16:19:41 +0500

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo datetimectl set-timezone Asia/Tashkent
+killall -r '^cms[A-Z]'
 sudo python3 setup.py install || exit 1
 sudo python3 prerequisites.py --yes --cmsuser=cmsuser install || exit 1
-killall -r '^cms[A-Z]'
 nohup cmsResourceService -a 1 & disown
 sleep 0.5s
