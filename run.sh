0fedc127f6c70e24b40a703a4b179e845cc939f7
drdilyor <drdilyor@outlook.com>
2024-04-21 16:19:05 +0500

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/bash
-cmsResourceService -a 1 &
-cmsLogService
+taskset -c 0 cmsContestWebServer 0 &
+taskset -c 0 cmsAdminWebServer 0 &
+
+taskset -c 0 cmsScoringService 0 &
+taskset -c 0 cmsEvaluationService 0 &
+taskset -c 0 cmsProxyWebServer 0 &
+
+taskset -c 1 cmsWorker 0 &
+taskset -c 2 cmsWorker 1 &
+taskset -c 3 cmsWorker 2 &
+
+taskset -c 0 cmsLogService 0 &
+taskset -c 0 cmsResourceService 0 &
