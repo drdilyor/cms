1d5dfe22cd60defeb9e01d266f830150d636c9ac
drdilyor <drdilyor@outlook.com>
2024-04-26 10:10:45 +0500

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+export TZ=Asia/Tashkent
 contest=3
 taskset -c 0 cmsContestWebServer 0 -c $contest &
 taskset -c 0 cmsAdminWebServer 0 -c $contest &
@@ -12,6 +13,5 @@ taskset -c 2 cmsWorker 1 &
 taskset -c 3 cmsWorker 2 &
 
 taskset -c 0 cmsLogService 0 &
-taskset -c 0 cmsResourceService 0 &
+taskset -c 0 cmsResourceService 0
 
-taskset -c 0 cmsRankingWebServer
